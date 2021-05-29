687a4f96c4f019aa49df1988129a9c13688e197e
nagendra0446 <nnagendra0446@gmail.com>
2021-05-29 11:45:45 +0530

    Changes Done

--- index.js
+++ index.js
@@ -3,28 +3,33 @@ const path = require('path');
 const useragent = require('express-useragent')
 const routes = require('./routes');
 const session = require('express-session');
-
 const app = express();
-
 const port = process.env.PORT || 5000;
+
+/* SETTING UP THE SERVER */
 
 app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(useragent.express());
+// app.use(useragent.express());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'secret key',
     resave: false,
     saveUninitialized: false
 }));
+
 app.use('/', routes);
+app.listen(port, ()=>{console.log(`Server running on port ${port.toString()}`)});
+
+/* ----------------------- */
 
 
+/* WRITING THE LOGIC FOR only get method of HOME PAGE */
+/* REMAINING ROUTES ARE IN routes.js*/
 
-// Home route
 app.get('/', (req, res) => {
     if(req.session.username){
         res.redirect('/dashboard');
@@ -32,4 +37,4 @@ app.get('/', (req, res) => {
     res.render('signin', {useragent});
 })
 
-app.listen(port, ()=>{console.log('Listening')});
+/* -------------------------------- */
